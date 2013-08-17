bf37d5b3c2f870a9187fecd9113c5b5539cd7db7
Chip <photex@gmail.com>
2013-08-16 17:58:40 -0700

    - it's working again.

--- clozure-defaults.scm
+++ clozure-defaults.scm
@@ -1,7 +1,4 @@
-;; Default project data definitions
-
-(define plant-default-config
-  '((#:no-user-init . "-n")
-    (#:load . "-l")
-    (#:eval . "-e")
-    (#:save . "'(save-application #P\".plant/~a-~a\" :prepend-kernel t :purify t)'")))
+((#:no-user-init . "-n")
+ (#:load . "-l")
+ (#:eval . "-e")
+ (#:save . "'(save-application #P\".plant/~a-~a\" :prepend-kernel t :purify t)'"))
--- sbcl-defaults.scm
+++ sbcl-defaults.scm
@@ -1,7 +1,4 @@
-;; Default project data definitions
-
-(define plant-default-config
-  '((#:no-user-init . "--no-userinit")
-    (#:load . "--load")
-    (#:eval . "--eval")
-    (#:save . "'(save-lisp-and-die #P\".plant/~a-~a\" :executable t :purify t)'")))
+((#:no-user-init . "--no-userinit")
+ (#:load . "--load")
+ (#:eval . "--eval")
+ (#:save . "'(save-lisp-and-die #P\".plant/~a-~a\" :executable t :purify t)'"))
