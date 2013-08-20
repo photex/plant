92e9ba3e8e8e569a3a26bae36d4f01f21499fb50
Chip Collier <photex@lofidelitygames.com>
2013-08-19 23:19:55 -0700

    blerg

--- plant.lisp
+++ plant.lisp
@@ -1,8 +1,28 @@
+(require :getopt)
+(require :cl-json)
 
-(defun main ()
-  (princ "Hello"))
+(use-package '(:cl-json))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(main)
-(quit)
+(defvar *default-quickloads* '("swank" "alexandria"))
+(defvar *plant-project* #P"")
+(defvar *plant-dir* #P"")
+(defvar *project-data* '())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun help (&optional cmd)
+  (format t "Here goes the help!~%"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun main (args)
+  ;; we need at least a command
+  (when (< (length args) 1)
+    (help)
+    (sys:exit 255))
+
+  (sys:exit 0))
+
+;; (main (cdr ext:*command-args*))
