6672b672f6b05a76871dba02a079f26c819efe0c
Chip <photex@gmail.com>
2013-09-26 17:39:45 -0700

    - update to lisp settings and project format
    I think plant isn't going to work right now but there has been great
    progress so far on the settings updates. Hopefully this will make it
    very easy to support multiple lisp versions and local dependencies.

---
+++ project-defaults.scm
@@ -0,0 +1,8 @@
+((#:quickloads . ("swank" "alexandria"))
+ (#:project-name . "unknown")
+ (#:project-version . "0.0.1")
+ (#:dependencies . ()))
+
+;;; dependency format
+;;; ((#:method . "git") ;; available methods are git, hg, www
+;;;  (#:url . "git://github.com/foo/bar.git")) ;; whatever the appropriate url would be for the chosen method
--- setup.lisp
+++ setup.lisp
@@ -1,6 +1,8 @@
 (ql:quickload :cffi)
 
+;;; setup.lisp is used to bootstrap some simple things in our saved lisp image.
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (let ((current-dir (truename ".")))
+  (let* ((current-dir (truename ".")))
     (pushnew current-dir cffi:*foreign-library-directories*)
     (pushnew current-dir asdf:*central-registry*)))
