b7a9757f619c6283b86683e3043d4078ddaa3909
Chip Collier <photex@lofidelitygames.com>
2013-12-22 08:02:47 -0800

    - the boostrap binary is in better shape now. sbcl and clisp tested. Clozure still pending.

--- build_plant.lisp
+++ build_plant.lisp
@@ -1,25 +1,58 @@
+;;; This file builds the top level plant binary.
+;;; It defines the core plant package and various parameters
+;;; that will be used by the plant runtime to perform its work.
 
-(defvar *plant-home*
-  (let ((ph-env-val #+clisp(ext:getenv "PLANT_HOME")
-                    #+sbcl(sb-ext:posix-getenv "PLANT_HOME")))
-    (handler-case (truename ph-env-val)
-      #+clisp
-      (system::simple-file-error ()
-        (truename (format nil "~a/" ph-env-val))))))
+(defpackage #:plant
+  (:use #:cl)
+  (:export #:*home*
+           #:boot))
 
-(format t "Loading ASDF~%")
-(load (merge-pathnames *plant-home* "asdf.lisp"))
-(pushnew (truename (format nil "~aslime/" *plant-home*))
+(in-package :plant)
+
+(defvar *home*
+  (let ((ph-env-val (uiop/os:getenv "PLANT_HOME")))
+    #-clisp (truename ph-env-val)
+    ;; TODO clisp raises an error when using truename
+    ;; on a string which points to a directory that doesn't
+    ;; end with a '/'. I need to learn a better way to take
+    ;; this env value and create a proper pathname from it.
+    #+clisp (handler-case (truename ph-env-val)
+              (system::simple-file-error ()
+                (truename (format nil "~a/" ph-env-val))))))
+
+;; This is our top level function which loads plant.lisp
+;; runs plant main, then exits.
+(defun boot ()
+  (let ((plant (merge-pathnames "plant.lisp" *home*)))
+    (load plant)
+    ;; TODO error handling, proper return codes, etc
+    (main)
+    ;(quit)
+    ))
+
+(in-package :cl-user)
+
+;; TODO This is the legacy method of telling asdf
+;; where to find systems. 
+(pushnew (truename (format nil "~aslime/" plant:*home*))
          asdf:*central-registry*)
+
 ;; For some reason I can't create-server if swank
-;; is loaded before we save the image. 
-#-clisp(asdf:load-system "swank")
+;; is loaded before we save the image with clisp.
+#-clisp(asdf:load-system "swank" :verbose nil)
+
+;; By this point we should have everything we need to
+;; save the binary.
+;; TODO is there an asdf/uiop function for getting argv?
 (let* ((args #+sbcl(cdr sb-ext:*posix-argv*)
              #+clisp ext:*args*)
-       (plant-bin (merge-pathnames *plant-home* (car args))))
+       (plant-bin (merge-pathnames plant:*home* (car args))))
   (format t "Saving plant binary~%")
+  ;; TODO uiop/image:create-image?
   #+sbcl
-  (save-lisp-and-die plant-bin :executable t)
+  (save-lisp-and-die plant-bin :executable t
+                     :toplevel #'plant:boot)
   #+clisp
-  (saveinitmem plant-bin :executable t :quiet t)
+  (saveinitmem plant-bin :executable t :quiet t
+               :init-function #'plant:boot)
   (quit))
--- plant.lisp
+++ plant.lisp
@@ -1,39 +1,27 @@
 ;; Can't use swank if it gets loaded before
 ;; you save an image in clisp. Have to load it
 ;; each run instead.
-#+clisp(asdf:load-system "swank")
+#+clisp(asdf:load-system "swank" :verbose nil)
+
+(in-package :plant)
 
 ;;;*plant-home* should be defined when plant is built
 ;;; but *plant-lisp* should be configurable by the environment.
-(defvar *plant-lisp*
+(defvar *lisp*
   (uiop/os:getenv "PLANT_LISP"))
 (defvar *rlwrap-p*
   (= 0 (asdf:run-shell-command "which rlwrap")))
 
-(defvar *args* ())
+;; due to the need to use '--' under clisp we just remove
+;; that item from the command line args when plant was built
+;; with another lisp.
+(defvar *args*
+  #-clisp (cdr (uiop:command-line-arguments))
+  #+clisp (uiop:command-line-arguments))
 
-#+sbcl
 (defun main ()
-  (let ((*args* (cddr sb-ext:*posix-argv*)))
-    (%main)))
-
-#+clozure
-(defun main ()
-  (format t "Clozure support is not implemented yet.~%")
-  (quit))
-
-#+clisp
-(defun main ()
-  (let ((*args* ext:*args*))
-    (%main)))
-
-(defun %main ()
-  (format t "PLANT_HOME=~a~%PLANT_LISP=~a~%" *plant-home* *plant-lisp*)
+  (format t "PLANT_HOME=~a~%PLANT_LISP=~a~%" *home* *lisp*)
   (when *rlwrap-p*
       (format t "rlwrap is available.~%"))
-  (swank:create-server :dont-close t)
   ;(quit)
   )
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (main))
